a934439207888e24caabf463d54569ae7f9897c1
960subhamsingh <kumarsubham0734@gmail.com>
2024-07-11 00:40:46 +0530

    list

---
+++ 04_Datatype/list.py
@@ -0,0 +1,10 @@
+# Lists
+my_list = list(range(0, 101, 2))  # Create the list
+print(my_list)
+
+print(my_list[0:8])  # Printing the first ten elements of the list
+
+print(len(my_list))  # Finding the length of the list
+
+my_list.append("Hii")  # Appending a value to this list - can be any type!
+print(my_list)
