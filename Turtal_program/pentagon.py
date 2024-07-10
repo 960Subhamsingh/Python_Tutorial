3c4e68a6350c6a38b76d54151803beb5019f58a1
960subhamsingh <kumarsubham0734@gmail.com>
2024-07-11 00:40:18 +0530

    pentagon

---
+++ Turtal_program/pentagon.py
@@ -0,0 +1,7 @@
+# Turtle
+import turtle
+
+for i in range(5):  # A pentagon has five sides!
+    turtle.right(72)  # This is the angle needed to produce a pentagon
+    turtle.forward(100)  # This will give the length of the pentagon sides
+turtle.done()
