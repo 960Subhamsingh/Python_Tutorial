84e24aa596d77d804368e548a115e895a95235a6
960Subhamsingh <158332737+960Subhamsingh@users.noreply.github.com>
2024-09-05 23:15:13 +0530

    triangle_pattern

--- pattern_program/tringle.py
+++ pattern_program/tringle.py
@@ -3,3 +3,10 @@ for i in range(1, num+1):
     for j in range(1, i+1):
         print("*", end=" ")
     print()
+
+
+num = int(input("Enter the number :"))
+for i in range(1, num+1):
+    for j in range(1, i+1):
+        print("1", end=" ")
+    print()   
