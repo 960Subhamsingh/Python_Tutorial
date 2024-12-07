3637bce1c8f597ae82714d9918c4316aa7b7b267
960Subhamsingh <158332737+960Subhamsingh@users.noreply.github.com>
2024-12-07 14:07:59 +0530

    circle

---
+++ Turtal_program/circle.py
@@ -0,0 +1,12 @@
+from turtle import*
+bgcolor("black")
+color("Orange")
+speed(0)
+for i in range(200):
+    goto(0,0)
+    fd(250)
+    lt(3)
+    circle(150, -90)
+    rt(90)
+    hideturtle()
+done()
