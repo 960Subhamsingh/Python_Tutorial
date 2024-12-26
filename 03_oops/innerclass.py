6d9102e8261026c35f6eca365e0d361b90d941ea
Subham Kumar <158332737+960Subhamsingh@users.noreply.github.com>
2024-12-26 12:58:07 +0530

    opps

---
+++ 03_oops/innerclass.py
@@ -0,0 +1,39 @@
+class Student:
+    def __init__(self, name, sem, com, ram):
+        self.name = name
+        self.sem = sem
+        # we can define inner class object into outer class like this
+        # self.com = self.Computer()
+        self.com = self.Computer(com, ram)
+
+    def show(self):
+        return self.name, self.sem, self.com.show()
+     # this is an inner class and it is used in only student class we declare inside it
+    class Computer:
+        ''' it is for example one when we are creating object in init method of outer class
+        def __init__(self):
+            self.company = 'Dell'
+            self.ram = '8GB' '''
+
+        def __init__(self, com, ram):
+            self.company = com
+            self.ram = ram
+
+        def show(self):
+            return self.company, self.ram
+    
+sub = Student("subham","4","Hp","8")
+sub= Student('dhaval', 8, 'Hp', '16GB')
+
+print("Show Student 1 : ", sub.show())
+print("Show Student 2 : ", sub.show())
+''' If we are defining an object in __init__ of outer class then we can access like this
+print("Computer company for student 1 : ",s1.com.company)
+print("Computer ram for student 2 : ",s2.com.ram) '''
+
+'''We can make object like this also of inner class
+com1 = Student.Computer('Dell','8GB')
+com2 = Student.Computer('Hp','16GB')
+print("Computer Details for student 1 : ",com1.show())
+print("Computer Details for student 2 : ",com2.show()) '''
+print("Show Student:" ,sub.show())
---
+++ 07_Function/module/calculator.py
@@ -0,0 +1,12 @@
+# Add Function
+def add(num1 : int , num2: int):
+    return num1 + num2
+# Add Function
+def sub(num1 : int , num2: int):
+    return num1 - num2
+# Add Function
+def mul(num1 : int , num2: int):
+    return num2* num1
+# Add Function
+def div(num1 : int , num2: int):
+    return  num1/num1
